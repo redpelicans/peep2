4b39561acca9d10c20c1d1c8138b497c59c6a2e1
eric <eric.basley@redpelicans.com>
2017-09-20 10:30:53 +0200

    Fix Companies > EmptySearch / Masonery

--- src/client/components/Companies/List.js
+++ src/client/components/Companies/List.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { isEmpty } from 'ramda';
-import Preview from './Preview';
-import MasonryLayout from '../widgets/MasonryLayout';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { isEmpty } from "ramda";
+import Preview from "./Preview";
+import MasonryLayout from "../widgets/MasonryLayout";
 
 const StyledContainer = styled.div`
   display: flex;
@@ -11,11 +11,11 @@ const StyledContainer = styled.div`
 `;
 
 export const EmptySearch = styled.span`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  height:70px;
+  height: 70px;
   align-self: center;
   justify-self: center;
   grid-column: left;
@@ -25,33 +25,32 @@ export const EmptySearch = styled.span`
 
 const sizes = [
   { columns: 1, gutter: 10 },
-  { mq: '800px', columns: 2, gutter: 10 },
-  { mq: '1100px', columns: 3, gutter: 10 },
-  { mq: '1400px', columns: 4, gutter: 10 },
-  { mq: '1700px', columns: 5, gutter: 10 },
+  { mq: "800px", columns: 2, gutter: 10 },
+  { mq: "1100px", columns: 3, gutter: 10 },
+  { mq: "1400px", columns: 4, gutter: 10 },
+  { mq: "1700px", columns: 5, gutter: 10 }
 ];
 
 export const List = ({ companies, ...params }) => (
   <StyledContainer>
-    <MasonryLayout
-      id="companies"
-      sizes={sizes}
-    >
-      {isEmpty(companies) &&
-        <EmptySearch>
-          <span className="pt-icon-large pt-icon-geosearch" />
-          No result...
-        </EmptySearch>}
-      {companies.map(companie => (
-        <Preview key={companie._id} companie={companie} {...params} />
-      ))}
-    </MasonryLayout>
+    {isEmpty(companies) ? (
+      <EmptySearch>
+        <span className="pt-icon-large pt-icon-geosearch" />
+        No result...
+      </EmptySearch>
+    ) : (
+      <MasonryLayout id="companies" sizes={sizes}>
+        {companies.map(companie => (
+          <Preview key={companie._id} companie={companie} {...params} />
+        ))}
+      </MasonryLayout>
+    )}
   </StyledContainer>
 );
 
 List.propTypes = {
   companies: PropTypes.array.isRequired,
-  filterCompanyList: PropTypes.func.isRequired,
+  filterCompanyList: PropTypes.func.isRequired
 };
 
 export default List;
