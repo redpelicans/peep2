a5361f60bf1c1b2dd63dd772fee482c33df32a2f
lcharvol <lcharvol@student.42.fr>
2017-09-21 17:44:58 +0200

    work on companie form

--- src/client/components/Companies/Add.js
+++ src/client/components/Companies/Add.js
@@ -1,159 +1,97 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { bindActionCreators } from 'redux';
+import { withState, withHandlers } from 'recompose';
 import { Button } from '@blueprintjs/core';
 import { Link } from 'react-router-dom';
-import { compose } from 'ramda';
+import { compose, map } from 'ramda';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
-import { AvatarSelector, textArea, renderField, renderSelect } from '../widgets';
-
-const Container = styled.div`
-  display:flex;
-  align-items: center;
-  flex-direction:column;
-  position:relative;
-  min-width:300px;
-  padding: 20px;
-  margin:25px;
-  padding-bottom:50px;
-  background-color: #394b59;
-  border-radius: 2px;
-`;
-
-const Header = styled.div`
-  display: flex;
-  padding-top:25px;
-  flex-wrap:wrap;
-  justify-content: space-between;
-  align-items: center;
-  width:90%;
-`;
-
-const Buttons = styled.div`
-  display:flex;
-  justify-content: flex-end;
-  align-items: center;
-  flex:1;
-`;
-
-const ButtonElt = styled(Button)`
-  height:30px;
-  margin-left:15px;
-  margin-right:15px;
-`;
-
-const TitleRow = styled.div`
-  display:flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.h3`
-  color:white;
-  margin:0;
-  margin-left:25px;
-`;
-
-const Form = styled.form`
-  display:flex;
-  flex-direction:column;
-  margin-top:25px;
-  width:90%;
-`;
-
-const InputRow = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin-top:10px;
-  width:100%;
-`;
+import { Header, HeaderLeft, HeaderRight } from '../Header';
+import getCities from '../../selectors/cities';
+import getCountries from '../../selectors/countries';
+import fields from './forms';
+import { Spacer, InputRow, TextInput, TextAreaInput, SelectInput, Title, Form, Container, AvatarSelector } from '../widgets';
 
 const validate = values => {
   const errors = {};
-  if (!values.Name) {
-    errors.Name = 'Required';
-  } if (!values.Website) {
-    errors.Website = 'Required';
-  } if (!values.City) {
-    errors.City = 'Required';
-  } if (!values.Country) {
-    errors.Country = 'Required';
-  }
+  map(field => {
+    if (field.required === true && !values[field.label]) {
+      errors[field.label] = 'Required';
+    }
+  }, fields);
   return errors;
 };
 
-class AddCompanie extends Component {
-  state = {
-    color: '',
+const getInputComponent = type => {
+  if (type === 'input') {
+    return TextInput;
+  } else if (type === 'textarea') {
+    return TextAreaInput;
+  } else if (type === 'select') {
+    return SelectInput;
   }
-  handleSubmit = (e) => {
-    console.log('submit: ', e);
-  }
-
-  handleChangeColor = color => {
-    this.setState({ color });
-  }
-
-  render() {
-    const { handleSubmit, valid, companieForm: { values = {} } } = this.props;
-    return (
-      <Container>
-        <Header>
-          <TitleRow>
-            <AvatarSelector handleChangeColor={this.handleChangeColor} name={values.Name} />
-            <Title>New Companie</Title>
-          </TitleRow>
-          <Buttons>
-            <ButtonElt form="companie" type="submit" disabled={!valid} className="pt-intent-success">Create</ButtonElt>
-            <Link to="/companies">
-              <ButtonElt className="pt-intent-warning">Cancel</ButtonElt>
-            </Link>
-          </Buttons>
-        </Header>
-        <Form id="companie" onSubmit={handleSubmit(this.handleSubmit)}>
-          <InputRow>
-            <Field name="Type" component={renderField} type="text" label="Type :" className="pt-input pt-dark" />
-            <Field name="Name" component={renderField} type="text" label="Name :" className="pt-input pt-dark" />
-            <Field name="Website" component={renderField} type="text" label="Website :" className="pt-input pt-dark" />
-          </InputRow>
-          <InputRow>
-            <Field name="Street" component={renderField} type="text" label="Street :" className="pt-input pt-dark" />
-            <Field name="Zip Code" component={renderField} type="text" label="Zip Code :" className="pt-input pt-dark" />
-          </InputRow>
-          <InputRow>
-            <Field name="City" component={renderSelect} label="City :" className="pt-input pt-dark" />
-            <Field name="Country" component={renderField} type="text" label="Country :" className="pt-input pt-dark" />
-          </InputRow>
-          <InputRow>
-            <Field name="Tags" component={renderField} type="text" label="Tags :" className="pt-input pt-dark" />
-          </InputRow>
-          <InputRow>
-            <Field name="Note" component={textArea} label="Note :" />
-          </InputRow>
-        </Form>
-      </Container>
-    );
-  }
-}
-
-AddCompanie.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  companieForm: PropTypes.object,
-  valid: PropTypes.bool,
 };
 
+const getFields = (cities, countries) =>
+  map(
+    field => (
+      <InputRow key={field.key}>
+        <Field component={getInputComponent(field.type)} name={field.key} field={field} cities={cities} countries={countries} />
+      </InputRow>
+    ),
+    fields,
+  );
+
+const AddCompanie = ({ changeColor, valid, cities, countries, companieForm: { values = {} } }) => (
+  <Container>
+    <Header>
+      <HeaderLeft>
+        <Spacer size={20} />
+        <AvatarSelector name={values.Name ? values.Name : ''} handleChangeColor={changeColor} />
+        <Spacer />
+        <Title title="New Companie" />
+      </HeaderLeft>
+      <HeaderRight>
+        <Button form="companie" type="submit" disabled={!valid} className="pt-intent-success">
+          Create
+        </Button>
+        <Spacer />
+        <Link to="/companies">
+          <Button className="pt-intent-warning">Cancel</Button>
+        </Link>
+        <Spacer size={25} />
+      </HeaderRight>
+    </Header>
+    <Form id="companie">{getFields(cities, countries)}</Form>
+  </Container>
+);
+
+AddCompanie.propTypes = {
+  changeColor: PropTypes.func.isRequired,
+  valid: PropTypes.bool,
+  cities: PropTypes.array,
+  countries: PropTypes.array,
+  companieForm: PropTypes.object,
+};
 
 const mapStateToProps = state => ({
-  countries: state.countries.data,
+  countries: getCountries(state),
+  cities: getCities(state),
   companieForm: state.form.companie,
 });
 
 const actions = {};
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
-export default compose(reduxForm({
-  form: 'companie',
-  validate,
-}), connect(mapStateToProps, mapDispatchToProps))(AddCompanie);
+const enhance = compose(
+  withState('color', 'changeColor', ''),
+  withHandlers({ changeColorHandler: ({ changeColor }) => () => changeColor(color => color) }),
+  reduxForm({
+    form: 'companie',
+    validate,
+  }),
+  connect(mapStateToProps, mapDispatchToProps),
+);
+
+export default enhance(AddCompanie);
---
+++ src/client/components/widgets/Form.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  margin: auto;
+  flex-direction: column;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  width: 90%;
+`;
+
+export const InputRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  width: 100%;
+`;
