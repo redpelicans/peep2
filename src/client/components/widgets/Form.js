26e813494eca36466b3dc168e3080641d7d8630c
lcharvol <lcharvol@student.42.fr>
2017-09-25 15:59:54 +0200

    Add company form responsivity

--- src/client/components/widgets/Form.js
+++ src/client/components/widgets/Form.js
@@ -8,5 +8,10 @@ export const CompagnyForm = styled.form`
   width: 90%;
   grid-auto-columns: minmax(100px, auto);
   grid-auto-rows: minmax(100px, auto);
-  grid-template-areas: 'Types Name Website' 'Zipcode Street Street' 'Country City City' 'Tags Tags Tags' 'Notes Notes Notes';
+  grid-template-areas: 'Types' 'Name' 'Website' 'Zipcode' 'Street' 'Country'
+    'City' 'Tags' 'Notes';
+  @media (min-width: 700px) {
+    grid-template-areas: 'Types Name Website' 'Zipcode Street Street'
+      'Country City City' 'Tags Tags Tags' 'Notes Notes Notes';
+  }
 `;
