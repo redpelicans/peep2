fe83ad7487d33832971492ab09e35ea919ea17a9
eric <eric.basley@redpelicans.com>
2017-10-03 17:04:50 +0200

    Use WithFormik

--- src/client/components/Agenda/Add.js
+++ src/client/components/Agenda/Add.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'ramda';
+import { prop, compose } from 'ramda';
 import { startOfDay, endOfDay, addWeeks, subWeeks } from 'date-fns';
 import { withState, withHandlers, lifecycle } from 'recompose';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Dialog, Button } from '@blueprintjs/core';
 import { bindActionCreators } from 'redux';
-import { Formik } from 'formik';
+import { withFormik } from 'formik';
 import { getCalendar } from '../../selectors/calendar';
 import { getWorker, getWorkers } from '../../selectors/people';
 import { getWorkerEventsByDate } from '../../selectors/events';
@@ -24,11 +24,16 @@ const Add = ({
   period,
   history,
   cancel,
-  addEvent,
   events,
   calendar,
   isDialogOpen,
   toggleDialog,
+  isSubmitting,
+  isValid,
+  handleReset,
+  handleSubmit,
+  dirty,
+  ...props
 }) => {
   if (!worker) return null;
   const calendarPeriod = [
@@ -36,105 +41,95 @@ const Add = ({
     endOfDay(addWeeks(period[1], 1)),
   ];
   const [minDate, maxDate] = calendarPeriod;
-  const initialValues = {
-    ...defaultValues,
-    period,
-    workerId: worker._id,
-  };
-  // getValidationSchema().isValid(initialValues) .then(x => console.log(x))
 
   return (
     <StyledContainer>
-      <Dialog isOpen={isDialogOpen} className="pt-dark">
-        <div className="pt-dialog-body">
-          Would you like to cancel this form?
-        </div>
-        <div className="pt-dialog-footer">
-          <div className="pt-dialog-footer-actions">
+      <div>
+        <Dialog isOpen={isDialogOpen} className="pt-dark">
+          <div className="pt-dialog-body">
+            Would you like to cancel this form?
+          </div>
+          <div className="pt-dialog-footer">
+            <div className="pt-dialog-footer-actions">
+              <Button
+                onClick={() => toggleDialog()}
+                className="pt-intent-warning pt-large"
+              >
+                {' '}
+                No{' '}
+              </Button>
+              <Button
+                onClick={() => history.goBack()}
+                className="pt-intent-success pt-large"
+              >
+                {' '}
+                Yes{' '}
+              </Button>
+            </div>
+          </div>
+        </Dialog>
+        <Header>
+          <HeaderLeft>
+            <div className="pt-icon-standard pt-icon-calendar" />
+            <Spacer />
+            <Title>New Event</Title>
+          </HeaderLeft>
+          <HeaderRight>
             <Button
-              onClick={() => toggleDialog()}
-              className="pt-intent-warning pt-large"
-            >
-              {' '}
-              No{' '}
-            </Button>
-            <Button
-              onClick={() => history.goBack()}
+              form="addEvent"
+              type="submit"
+              disabled={isSubmitting}
               className="pt-intent-success pt-large"
             >
-              {' '}
-              Yes{' '}
+              Create
             </Button>
-          </div>
-        </div>
-      </Dialog>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={getValidationSchema()}
-        onSubmit={addEvent}
-        render={({ isSubmitting, isValid, handleReset, dirty, ...props }) => {
-          return (
-            <div>
-              <Header>
-                <HeaderLeft>
-                  <div className="pt-icon-standard pt-icon-calendar" />
-                  <Spacer />
-                  <Title>New Event</Title>
-                </HeaderLeft>
-                <HeaderRight>
-                  <Button
-                    form="addEvent"
-                    type="submit"
-                    disabled={isSubmitting}
-                    className="pt-intent-success pt-large"
-                  >
-                    Create
-                  </Button>
-                  <Spacer />
-                  <Button
-                    className="pt-intent-warning pt-large"
-                    onClick={cancel(dirty)}
-                  >
-                    Cancel
-                  </Button>
-                  <Spacer />
-                  <Button
-                    className="pt-intent-danger pt-large"
-                    onClick={handleReset}
-                    disabled={!dirty || isSubmitting}
-                  >
-                    {' '}
-                    Reset{' '}
-                  </Button>
-                </HeaderRight>
-              </Header>
-              <AddOrEdit
-                worker={worker}
-                minDate={minDate}
-                maxDate={maxDate}
-                calendar={calendar}
-                event={event}
-                events={events}
-                {...props}
-              />
-            </div>
-          );
-        }}
-      />
+            <Spacer />
+            <Button
+              className="pt-intent-warning pt-large"
+              disabled={isSubmitting}
+              onClick={cancel(dirty)}
+            >
+              Cancel
+            </Button>
+            <Spacer />
+            <Button
+              className="pt-intent-danger pt-large"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}
+            >
+              Reset
+            </Button>
+          </HeaderRight>
+        </Header>
+        <AddOrEdit
+          worker={worker}
+          minDate={minDate}
+          maxDate={maxDate}
+          calendar={calendar}
+          event={event}
+          events={events}
+          handleSubmit={handleSubmit}
+          {...props}
+        />
+      </div>
     </StyledContainer>
   );
 };
 
 Add.propTypes = {
-  addEvent: PropTypes.func.isRequired,
   period: PropTypes.array.isRequired,
   history: PropTypes.object.isRequired,
   worker: PropTypes.object.isRequired,
   calendar: PropTypes.object,
-  events: PropTypes.object.isRequired,
+  events: PropTypes.object,
   cancel: PropTypes.func.isRequired,
   toggleDialog: PropTypes.func.isRequired,
   isDialogOpen: PropTypes.bool.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  handleReset: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  dirty: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state, { history }) => {
@@ -160,9 +155,17 @@ const componentLifecycle = {
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
   lifecycle(componentLifecycle),
+  withFormik({
+    handleSubmit: console.log,
+    validationSchema: getValidationSchema(),
+    mapPropsToValues: ({ worker, period }) => ({
+      ...defaultValues,
+      period,
+      workerId: prop('_id', worker || {}),
+    }),
+  }),
   withState('isDialogOpen', 'showDialog', false),
   withHandlers({
-    addEvent: () => values => console.log(values),
     cancel: ({ history, showDialog }) => dirty => () => {
       if (!dirty) return history.goBack();
       return showDialog(isDialogOpen => !isDialogOpen);
