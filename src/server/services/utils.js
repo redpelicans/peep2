88b039c77473de5e36a604366da5de70a4be9da3
eric <eric.basley@redpelicans.com>
2017-10-03 17:08:00 +0200

    Replace Formik by withFormik

--- src/server/services/utils.js
+++ src/server/services/utils.js
@@ -1,14 +1,27 @@
-export const formatOutput = maker => (ctx) => {
+import Joi from 'joi';
+
+export const formatOutput = maker => ctx => {
   const { output } = ctx;
   return Promise.resolve({ ...ctx, output: maker(output) });
 };
 
-export const formatInput = maker => (ctx) => {
+export const formatInput = maker => ctx => {
   const { input } = ctx;
   return Promise.resolve({ ...ctx, input: maker(input) });
 };
 
-export const checkUser = () => (ctx) => {
+export const validate = schema => ctx => {
+  const { input } = ctx;
+  const promise = new Promise((resolve, reject) => {
+    Joi.validate(input, schema, (err, value) => {
+      if (err) return reject(err);
+      resolve(ctx);
+    });
+  });
+  return promise;
+};
+
+export const checkUser = () => ctx => {
   if (ctx.user) return Promise.resolve(ctx);
   return Promise.reject({ code: 403, error: 'Forbidden access' });
 };
