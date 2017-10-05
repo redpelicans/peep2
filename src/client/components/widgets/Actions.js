8a9dcdf9cb449f78cc9c3ecfe7e49c5110d5829e
lcharvol <lcharvol@student.42.fr>
2017-10-05 07:38:14 +0200

    Fix Actions widget UI

--- src/client/components/widgets/Actions.js
+++ src/client/components/widgets/Actions.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 const Actions = styled.div`
   display: flex;
-  right: 0;
-  top: 0;
+  justify-content: center;
+  align-items: center;
+  right: 10px;
+  top: 10px;
   flex: 1;
   z-index: 5;
   position: absolute;
