49074c57668a8c07bb638be5f577e4c9adbfe839
Mehdi AISSA-BRAHIM <maissa-b@e3r4p22.42.fr>
2017-10-06 19:32:26 +0200

    Tests > selectors > tags

--- src/client/selectors/tags.js
+++ src/client/selectors/tags.js
@@ -19,27 +19,29 @@ import {
   uniq,
 } from 'ramda';
 import { createSelector } from 'reselect';
+import { getCompanies } from './companies';
+import { getPeople } from './people';
 
-const getCompanies = state => state.companies.data;
-const getPeople = state => state.people.data;
-const getFilter = state => state.tags.filter;
+export const getFilter = state => state.tags.filter;
 
-const getFlattenTags = compose(flatten, pluck('tags'), values);
-const mergeTags = compose(
+export const getFlattenTags = compose(flatten, pluck('tags'), values);
+export const mergeTags = compose(
   filter(identity),
   reduce((tags, list) => concat(tags, getFlattenTags(list)), []),
 );
-const doSort = sort(descend(prop(1)));
-const doFormat = compose(
+export const doSort = sort(descend(prop(1)));
+export const doFormat = compose(
   map(([tag, elts]) => [tag, elts.length]),
   toPairs,
   groupBy(identity),
 );
-const regexp = filter => new RegExp(filter, 'i');
-const doFilter = tfilter => filter(x => !isEmpty(match(regexp(tfilter), x)));
-const groupByFilterAndSort = (filter, ...lists) =>
+export const regexp = filter => new RegExp(filter, 'i');
+export const doFilter = tfilter =>
+  filter(x => !isEmpty(match(regexp(tfilter), x)));
+export const groupByFilterAndSort = (filter, ...lists) =>
   compose(doSort, doFormat, doFilter(filter))(mergeTags(lists));
-const allTags = (...lists) => compose(sortBy(identity), uniq)(mergeTags(lists));
+export const allTags = (...lists) =>
+  compose(sortBy(identity), uniq)(mergeTags(lists));
 
 export const getVisibleTags = createSelector(
   [getFilter, getCompanies, getPeople],
