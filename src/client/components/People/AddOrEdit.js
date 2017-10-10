99af1c62ed200a6be0c4ff434140652b8acbb0fd
lcharvol <lcharvol@student.42.fr>
2017-10-10 16:24:22 +0200

    Fixes #98

--- src/client/components/People/AddOrEdit.js
+++ src/client/components/People/AddOrEdit.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FormField } from '../../fields';
-import { getField } from '../../forms/peoples';
+import { getField } from '../../forms/people';
 
 const PeopleForm = styled.form`
   display: grid;
