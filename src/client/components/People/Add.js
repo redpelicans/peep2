67430bfa8744cca967e5a52f4d2119675ab32738
lcharvol <lcharvol@student.42.fr>
2017-10-11 14:04:30 +0200

    Fix

--- src/client/components/People/Add.js
+++ src/client/components/People/Add.js
@@ -9,7 +9,7 @@ import { connect } from 'react-redux';
 import { Header, HeaderLeft, HeaderRight } from '../Header';
 import { withFormik } from 'formik';
 import { getValidationSchema, defaultValues } from '../../forms/people';
-import { addPeople, checkEmail } from '../../actions/people';
+import { addPeople } from '../../actions/people';
 import { Prompt } from 'react-router';
 import {
   Spacer,
@@ -22,7 +22,104 @@ import AddOrEdit from './AddOrEdit';
 
 export const StyledContainer = styled(Container)`min-width: 300px;`;
 
-const Add = compose(
+export const Add = ({
+  values,
+  isSubmitting,
+  isValid,
+  dirty,
+  handleSubmit,
+  handleReset,
+  setFieldTouched,
+  setFieldValue,
+  isCancelDialogOpen,
+  showCancelDialog,
+  cancel,
+  requestCancel,
+  ...props
+}) => (
+  <StyledContainer>
+    <Prompt
+      when={!isCancelDialogOpen && dirty && !isSubmitting}
+      message="Would you like to cancel this form ?"
+    />
+    <ModalConfirmation
+      isOpen={isCancelDialogOpen}
+      title="Would you like to cancel this form ?"
+      reject={() => showCancelDialog(false)}
+      accept={cancel}
+    />
+    <Header>
+      <HeaderLeft>
+        <Spacer size={15} />
+        <AvatarSelector
+          formId="peopleForm"
+          color={values.color}
+          name={values.firstName}
+          lastName={values.lastName}
+          setFieldTouched={setFieldTouched}
+          setFieldValue={setFieldValue}
+        />
+        <Spacer />
+        <Title title={'Add People'} />
+      </HeaderLeft>
+      <HeaderRight>
+        <Button
+          form="peopleForm"
+          type="submit"
+          disabled={isSubmitting || !isValid || !dirty}
+          className="pt-intent-success pt-large"
+        >
+          Create
+        </Button>
+        <Spacer />
+        <Button
+          onClick={requestCancel(dirty)}
+          className="pt-intent-warning pt-large"
+        >
+          Cancel
+        </Button>
+        <Spacer />
+        <Button
+          className="pt-intent-danger pt-large"
+          onClick={handleReset}
+          disabled={!dirty || isSubmitting}
+        >
+          Reset
+        </Button>
+        <Spacer size={20} />
+      </HeaderRight>
+    </Header>
+    <AddOrEdit
+      type="add"
+      handleSubmit={handleSubmit}
+      values={values}
+      setFieldTouched={setFieldTouched}
+      setFieldValue={setFieldValue}
+      {...props}
+    />
+  </StyledContainer>
+);
+
+Add.propTypes = {
+  isSubmitting: PropTypes.bool.isRequired,
+  isValid: PropTypes.bool.isRequired,
+  handleReset: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  dirty: PropTypes.bool.isRequired,
+  values: PropTypes.object.isRequired,
+  setFieldTouched: PropTypes.func.isRequired,
+  setFieldValue: PropTypes.func.isRequired,
+  showCancelDialog: PropTypes.func.isRequired,
+  isCancelDialogOpen: PropTypes.bool.isRequired,
+  cancel: PropTypes.func.isRequired,
+  requestCancel: PropTypes.func.isRequired,
+};
+
+const actions = { addPeople };
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default compose(
+  connect(null, mapDispatchToProps),
   withFormik({
     handleSubmit: (
       {
@@ -62,7 +159,7 @@ const Add = compose(
       addPeople(newPeople);
       history.goBack();
     },
-    validationSchema: getValidationSchema({ email: { validate: checkEmail } }),
+    validationSchema: getValidationSchema(),
     mapPropsToValues: () => ({
       ...defaultValues,
     }),
@@ -77,111 +174,4 @@ const Add = compose(
     toggleDialog: ({ showDialog }) => () =>
       showDialog(isDialogOpen => !isDialogOpen),
   }),
-)(
-  ({
-    values,
-    isSubmitting,
-    isValid,
-    dirty,
-    handleSubmit,
-    handleReset,
-    setFieldTouched,
-    setFieldValue,
-    isCancelDialogOpen,
-    showCancelDialog,
-    cancel,
-    requestCancel,
-    ...props
-  }) => (
-    <StyledContainer>
-      {console.log('values: ', values)}
-      <Prompt
-        when={!isCancelDialogOpen && dirty && !isSubmitting}
-        message="Would you like to cancel this form ?"
-      />
-      <ModalConfirmation
-        isOpen={isCancelDialogOpen}
-        title="Would you like to cancel this form ?"
-        reject={() => showCancelDialog(false)}
-        accept={cancel}
-      />
-      <Header>
-        <HeaderLeft>
-          <Spacer size={15} />
-          <AvatarSelector
-            formId="peopleForm"
-            color={values.color}
-            name={values.firstName}
-            lastName={values.lastName}
-            setFieldTouched={setFieldTouched}
-            setFieldValue={setFieldValue}
-          />
-          <Spacer />
-          <Title title={'Add People'} />
-        </HeaderLeft>
-        <HeaderRight>
-          <Button
-            form="peopleForm"
-            type="submit"
-            disabled={isSubmitting || !isValid || !dirty}
-            className="pt-intent-success pt-large"
-          >
-            Create
-          </Button>
-          <Spacer />
-          <Button
-            onClick={requestCancel(dirty)}
-            className="pt-intent-warning pt-large"
-          >
-            Cancel
-          </Button>
-          <Spacer />
-          <Button
-            className="pt-intent-danger pt-large"
-            onClick={handleReset}
-            disabled={!dirty || isSubmitting}
-          >
-            Reset
-          </Button>
-          <Spacer size={20} />
-        </HeaderRight>
-      </Header>
-      <AddOrEdit
-        type="add"
-        handleSubmit={handleSubmit}
-        values={values}
-        setFieldTouched={setFieldTouched}
-        setFieldValue={setFieldValue}
-        {...props}
-      />
-    </StyledContainer>
-  ),
-);
-
-Add.propTypes = {
-  isSubmitting: PropTypes.bool.isRequired,
-  isValid: PropTypes.bool.isRequired,
-  handleReset: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  dirty: PropTypes.bool.isRequired,
-  values: PropTypes.object.isRequired,
-  setFieldTouched: PropTypes.func.isRequired,
-  setFieldValue: PropTypes.func.isRequired,
-  showCancelDialog: PropTypes.func.isRequired,
-  isCancelDialogOpen: PropTypes.bool.isRequired,
-  cancel: PropTypes.func.isRequired,
-  requestCancel: PropTypes.func.isRequired,
-};
-
-const actions = { addPeople, checkEmail };
-const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
-
-const FormikAdd = ({ checkEmail, ...props }) => (
-  <Add checkEmail={checkEmail} {...props} />
-);
-
-FormikAdd.propTypes = {
-  checkEmail: PropTypes.func.isRequired,
-};
-
-export default connect(null, mapDispatchToProps)(FormikAdd);
+)(Add);
