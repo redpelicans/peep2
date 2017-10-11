239ede8ffeb97c0f4abe0c376bf4920b673926ff
maissa-b <maissa-b@student.42.fr>
2017-10-11 20:34:25 +0200

    Tests > components > widgets > LinkButton

--- src/client/components/widgets/LinkButton.js
+++ src/client/components/widgets/LinkButton.js
@@ -4,7 +4,7 @@ import { Button } from '@blueprintjs/core';
 import { compose, withHandlers } from 'recompose';
 import { withRouter } from 'react-router';
 
-const LinkButton = ({ iconName, className, handleClick }) => (
+export const LinkButton = ({ iconName, className, handleClick }) => (
   <Button iconName={iconName} onClick={handleClick} className={className} />
 );
 
