bb3c981a2affe040b4971d1ba98b08a2c0d1b9ee
lcharvol <lcharvol@student.42.fr>
2017-10-12 07:53:04 +0200

    Add delete notes action

--- src/client/actions/notes.js
+++ src/client/actions/notes.js
@@ -1,9 +1,10 @@
 export const LOAD_NOTES = 'EvtX:Server:notes:load';
 export const NOTE_ADDED = 'note:added';
-export const NOTES_DELETED = 'notes:deleted';
 export const NOTES_LOADED = 'notes:loaded';
 export const FILTER_NOTES_LIST = 'filter:notes:list';
 export const SORT_NOTES_LIST = 'sort:notes:list';
+export const DELETE_NOTES = 'EvtX:Server:notes:del';
+export const NOTES_DELETED = 'notes:deleted';
 
 export const filterNotesList = filter => ({
   type: FILTER_NOTES_LIST,
@@ -20,4 +21,12 @@ export const loadNotes = () => (dispatch, getState) => {
       replyTo: NOTES_LOADED,
     });
   }
+};
+
+export const deleteNote = id => dispatch => {
+  dispatch({
+    type: DELETE_NOTES,
+    payload: { _id: id },
+    replyTo: NOTES_DELETED,
+  });
 };
