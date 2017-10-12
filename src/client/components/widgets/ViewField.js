6d959026c8eb5800621c59d27eb823e4c105b173
eric <eric.basley@redpelicans.com>
2017-10-12 17:57:37 +0200

    Update ViewField css
    * use flexbox to center
    * add overflow and ellipsis rules

--- src/client/components/widgets/ViewField.js
+++ src/client/components/widgets/ViewField.js
@@ -4,26 +4,41 @@ import PropTypes from 'prop-types';
 import { Colors } from '@blueprintjs/core';
 
 const StyledBlock = styled.div`
-  text-align: center;
+  padding-left: 0.8em;
+  padding-right: 0.8em;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
   height: 30px;
   background-color: ${Colors.DARK_GRAY3};
   border-radius: 3px;
-  margin-top: 10px;
+  margin-top: 15px;
+  height: 35px;
   box-shadow: 0 1px 0 rgba(0, 0, 0, 0.07);
 `;
 
 const StyledBlockContent = styled.div`
-  padding-top: 5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
   color: ${Colors.LIGHT_GRAY5} !important;
 `;
 
+const StyledFormField = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const ViewField = ({ label, value, className }) => (
-  <div className={className}>
+  <StyledFormField className={className}>
     <label>{label}</label>
     <StyledBlock>
-      {value && <StyledBlockContent>{value}</StyledBlockContent>}
+      {value && (
+        <StyledBlockContent>
+          <span>{value}</span>
+        </StyledBlockContent>
+      )}
     </StyledBlock>
-  </div>
+  </StyledFormField>
 );
 
 ViewField.propTypes = {
