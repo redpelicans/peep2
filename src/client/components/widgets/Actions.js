3523768f45302bef231081c1d816cdfc300da9f7
eric <eric.basley@redpelicans.com>
2017-10-13 14:21:44 +0200

    Tune transparency

--- src/client/components/widgets/Actions.js
+++ src/client/components/widgets/Actions.js
@@ -14,7 +14,7 @@ const Actions = styled.div`
   position: absolute;
   background: linear-gradient(
     to right,
-    rgba(48, 64, 77, 0),
+    rgba(48, 64, 77, 0.5),
     rgba(48, 64, 77, 1)
   );
 `;
