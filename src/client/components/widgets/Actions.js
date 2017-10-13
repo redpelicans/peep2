6bd9d3dcce82eff278ac4017852fe22379402d04
eric <eric.basley@redpelicans.com>
2017-10-13 14:28:48 +0200

    Use radial gradient

--- src/client/components/widgets/Actions.js
+++ src/client/components/widgets/Actions.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+//  background: linear-gradient( to right, rgba(48, 64, 77, 0.5), rgba(48, 64, 77, 1)
 const Actions = styled.div`
   display: flex;
   flex-direction: row;
@@ -12,10 +13,10 @@ const Actions = styled.div`
   width: 100%;
   z-index: 5;
   position: absolute;
-  background: linear-gradient(
-    to right,
-    rgba(48, 64, 77, 0.5),
-    rgba(48, 64, 77, 1)
+  background: radial-gradient(
+    circle at top right,
+    rgba(48, 64, 77, 1),
+    rgba(48, 64, 77, 0.3)
   );
 `;
 
