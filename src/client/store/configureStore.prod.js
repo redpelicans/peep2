7dca7daf2762a5f95d17ca231d7ad5de6bf2fe9e
eric <eric.basley@redpelicans.com>
2017-10-25 14:54:39 +0200

    Remove redux logger in production

--- src/client/store/configureStore.prod.js
+++ src/client/store/configureStore.prod.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
-import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { socketIoMiddleWare } from '../middlewares';
 
-const configureStore = (initialState, io) => createStore(
-  rootReducer,
-  initialState,
-  applyMiddleware(socketIoMiddleWare(io), thunk, createLogger),
-);
+const configureStore = (initialState, io) =>
+  createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(socketIoMiddleWare(io), thunk),
+  );
 
 export default configureStore;
