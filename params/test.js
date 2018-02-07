c483bc827cec7490385a16251223c84fa992ccda
Thomas Tridon <thomastridon@macbook-pro-de-pauline-2.home>
2018-02-07 12:15:34 +0100

    I175: update node packages partially
    * full update

--- params/test.js
+++ params/test.js
@@ -1,17 +1,17 @@
 module.exports = {
   db: {
-    host: '0.0.0.0',
+    host: 'rp3.redpelicans.com',
     port: 27017,
     options: {
       auto_reconnect: true,
       poolSize: 10,
       w: 1,
       strict: true,
-      native_parser: true
+      native_parser: true,
     },
     database: 'test',
   },
   server: {
     host: '127.0.0.1',
-  }
-}
+  },
+};
