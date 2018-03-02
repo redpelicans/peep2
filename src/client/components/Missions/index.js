5763bae06a0be9948f55aa6eb0d6ada77f47ae12
Thomas Tridon <thomastridon@macbook-pro-de-pauline-2.home>
2018-03-02 12:41:48 +0100

    I187 improve UI

--- src/client/components/Missions/index.js
+++ src/client/components/Missions/index.js
@@ -52,43 +52,82 @@ export const Missions = ({
   filterMissionsList,
   onFilterChange,
   sortMissionsList,
-}) => (
-  <Container>
-    <Header>
-      <HeaderLeft>
-        <div className="pt-icon-standard pt-icon-shopping-cart" />
-        <Spacer />
-        <Title title="Missions" />
-      </HeaderLeft>
-      <HeaderRight>
-        <Search
-          filter={filter}
-          onChange={onFilterChange}
-          resetValue={() => filterMissionsList('')}
-        />
-        <Spacer />
-        <Popover position={Position.BOTTOM_RIGHT}>
-          <Button className="pt-minimal" iconName="pt-icon-menu" />
-          <Menu>
-            <MenuDivider title="Mission" />
-            <LinkButton to={getPathByName('addMission')} iconName="plus" />
-            <SortMenu
-              sortTypes={sortTypes}
-              onClick={sortMissionsList}
-              sort={sort}
-            />
-          </Menu>
-        </Popover>
-      </HeaderRight>
-    </Header>
-    <List
-      people={people}
-      companies={companies}
-      missions={missions}
-      deleteMission={deleteMission}
-    />
-  </Container>
-);
+}) => {
+  console.log(sort);
+  return (
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <div className="pt-icon-standard pt-icon-shopping-cart" />
+          <Spacer />
+          <Title title="Missions" />
+        </HeaderLeft>
+        <HeaderRight>
+          <Search
+            filter={filter}
+            onChange={onFilterChange}
+            resetValue={() => filterMissionsList('')}
+          />
+          <Spacer />
+          <Popover position={Position.BOTTOM_RIGHT}>
+            <Button className="pt-minimal" iconName="pt-icon-menu" />
+            <Menu>
+              <MenuDivider title="Mission" />
+              <LinkButton
+                className="pt-minimal"
+                to={getPathByName('addMission')}
+                iconName="pt-icon-add"
+                text="Add"
+              />
+              <MenuItem iconName="pt-icon-double-caret-vertical" text="Sort" />
+              <ButtonGroup className="pt-minimal">
+                <Button
+                  iconName={
+                    sort.by === 'name'
+                      ? sort.order === 'asc'
+                        ? 'pt-icon-caret-up'
+                        : 'pt-icon-caret-down'
+                      : null
+                  }
+                  text="name"
+                  onClick={() => sortMissionsList('name')}
+                />
+                <Button
+                  iconName={
+                    sort.by === 'startDate'
+                      ? sort.order === 'asc'
+                        ? 'pt-icon-caret-up'
+                        : 'pt-icon-caret-down'
+                      : null
+                  }
+                  text="start date"
+                  onClick={() => sortMissionsList('startDate')}
+                />
+                <Button
+                  iconName={
+                    sort.by === 'endDate'
+                      ? sort.order === 'asc'
+                        ? 'pt-icon-caret-up'
+                        : 'pt-icon-caret-down'
+                      : null
+                  }
+                  text="end date"
+                  onClick={() => sortMissionsList('endDate')}
+                />
+              </ButtonGroup>
+            </Menu>
+          </Popover>
+        </HeaderRight>
+      </Header>
+      <List
+        people={people}
+        companies={companies}
+        missions={missions}
+        deleteMission={deleteMission}
+      />
+    </Container>
+  );
+};
 
 Missions.propTypes = {
   people: PropTypes.object,
