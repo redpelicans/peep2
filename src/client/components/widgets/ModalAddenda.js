e8311c635d55023a07a4f5e50ac6fa61ef15a8d2
Thomas TRIDON <ttridon@e2r5p19.42.fr>
2018-03-05 17:49:23 +0100

    I189: Widget fixed

--- src/client/components/widgets/ModalAddenda.js
+++ src/client/components/widgets/ModalAddenda.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import ReactMarkdown from 'react-markdown';
 import { Dialog } from '@blueprintjs/core';
 import FormikAdd from '../Addenda/Add';
 import FormikEdit from '../Addenda/Edit';
@@ -10,7 +9,6 @@ const DialogStyled = styled(Dialog)`width: 70%;`;
 
 const ModalAddenda = ({
   isOpen,
-  value,
   type,
   defaultValues,
   accept,
@@ -32,7 +30,6 @@ const ModalAddenda = ({
 
 ModalAddenda.propTypes = {
   isOpen: PropTypes.bool,
-  value: PropTypes.object,
   type: PropTypes.string.isRequired,
   accept: PropTypes.func.isRequired,
   reject: PropTypes.func.isRequired,
