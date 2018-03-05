a923ace41b5e5be05e96b2ab775a73e4c13ce7db
Thomas TRIDON <ttridon@e2r5p19.42.fr>
2018-03-05 18:53:14 +0100

    I189: evtx / http fixed

--- src/server/evtx/index.js
+++ src/server/evtx/index.js
@@ -5,7 +5,7 @@ import initRestApi from '../restApi';
 
 const loginfo = debug('peep:evtx');
 const init = ctx => {
-  const { io, config } = ctx;
+  const { config } = ctx;
   const evtx = evtX(config).configure(initServices);
   const restApi = evtX(config).configure(initRestApi);
   loginfo('EvtX setup.');
