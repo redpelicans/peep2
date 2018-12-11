587f20bf9805350c73487873dc63b8c8fc038ebd
eric-basley <eric-basley@users.noreply.github.com>
2018-12-11 17:11:51 +0100

    Add 2019 spare days

--- src/server/services/calendar/spare_days.js
+++ src/server/services/calendar/spare_days.js
@@ -21,6 +21,17 @@ const daysOff = [
   [d(2018, 10, 1), 'La Toussaint'],
   [d(2018, 10, 11), 'Armistice 1918'],
   [d(2018, 11, 25), 'Noẽl'],
+  [d(2019, 0, 1), "Jour de l'an"],
+  [d(2019, 3, 22), 'Lundi de Pâques'],
+  [d(2019, 4, 1), 'Fête du Travail'],
+  [d(2019, 4, 8), '8 mai 1945'],
+  [d(2018, 4, 30), 'Ascension'],
+  [d(2019, 5, 10), 'Pentecote'],
+  [d(2019, 6, 14), 'Fête Nationale'],
+  [d(2019, 7, 15), 'Assomption'],
+  [d(2019, 10, 1), 'La Toussaint'],
+  [d(2019, 10, 11), 'Armistice 1918'],
+  [d(2019, 11, 25), 'Noẽl'],
 ];
 
 const exportedDays = map(([date, label]) => [dmy(date), label])(daysOff);
